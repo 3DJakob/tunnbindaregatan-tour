3e1547266fcf059fa8ecb7993080c21ee10b97ff
Jakob Unnebäck <jakob@folkdatorn.se>
2021-08-11 22:36:42 +0200

    Barebones concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
 import React, { ReactElement } from 'react'
 import Tour from './components/Tour'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  text-align: center;
+`
 
 function App (): ReactElement {
   return (
     <div className='App'>
+      <Title>360 tour demo</Title>
       <Tour />
     </div>
   )
--- src/components/Tour.tsx
+++ src/components/Tour.tsx
@@ -1,14 +1,45 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import styled from 'styled-components'
+import { PanoViewer } from '@egjs/view360'
+import image1 from '../images/1.jpg'
+import image2 from '../images/2.jpg'
+import image3 from '../images/3.jpg'
 
 export interface TourProps {
 
 }
 
-const Tour: React.FC<TourProps> = ({ }) => {
+const PanViewer = styled.div`
+    height: 100vh;
+    position: relative;
+`
+
+const Tour: React.FC<TourProps> = () => {
+  const initTour = (): void => {
+    // create PanoViewer with option
+    const target: HTMLElement | null = document.getElementById('myPanoViewer')
+    if (target !== null) {
+      const targetElement: HTMLElement = target
+      const panoViewer = new PanoViewer(
+        targetElement,
+        {
+        //   image: '/360/test/1.jpg',
+          image: image1,
+          width: 300,
+          height: 300,
+          gyroMode: 'yawPitch'
+        }
+      )
+    }
+  }
+
+  useEffect(() => {
+    initTour()
+  }, [])
+
   return (
     <div>
-      Tour
+      <PanViewer id='myPanoViewer' />
     </div>
   )
 }
