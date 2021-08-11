4ad80f2c7662946797fb3dfadfa6bae909f079ad
Jakob Unnebäck <jakob@folkdatorn.se>
2021-08-11 23:11:39 +0200

    initial setup

---
+++ data.js
@@ -0,0 +1,138 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-1",
+      "name": "1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "yaw": -1.0883965619278158,
+        "pitch": 0.19009145375016523,
+        "fov": 1.4334316443869837
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.22713756151209807,
+          "pitch": 0.45294137191049444,
+          "rotation": 0,
+          "target": "1-2"
+        },
+        {
+          "yaw": -2.098551042422752,
+          "pitch": 0.42215474421504595,
+          "rotation": 0,
+          "target": "2-3"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-2",
+      "name": "2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.878940824050156,
+          "pitch": 0.45047533847577625,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-3",
+      "name": "3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.550478902899828,
+          "pitch": 0.4375436186513184,
+          "rotation": 0,
+          "target": "0-1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.7391513471706048,
+          "pitch": 0.16387302365545153,
+          "title": "Garderob",
+          "text": "Hejsan, detta är en garderob..."
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
