c484a8331f6fd92f3ff587a10edc595d85d388b3
Jakob Unnebäck <jakob@folkdatorn.se>
2021-08-11 22:04:20 +0200

    Project setup

---
+++ src/components/Tour.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+export interface TourProps {
+
+}
+
+const Tour: React.FC<TourProps> = ({ }) => {
+  return (
+    <div>
+      Tour
+    </div>
+  )
+}
+
+export default Tour
